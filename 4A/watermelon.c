1eb79ac23fd0e02591bb991af293ffc531670d15
Sly Kint A. Bacalso <64179667+Son0fAnton@users.noreply.github.com>
2021-08-21 20:20:26 +0800

    Create watermelon.c

---
+++ 4A/watermelon.c
@@ -0,0 +1,14 @@
+//watermelon
+#include <stdio.h>
+ 
+int main(){
+    int w;
+    scanf("%d", &w);
+    if(w <= 2 ){
+        printf("NO");
+    } else if(w % 2 == 0){
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+}
